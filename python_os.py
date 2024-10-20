175f027a36f68f323c9e202a886ebf49d8e7e701
Nadezda711 <semiliya711@mail.ru>
2024-10-20 14:46:52 +0500

    Add files via upload

---
+++ python_os.py
@@ -0,0 +1,40 @@
+import os
+import time
+from os import chdir
+
+directory = os.getcwd()
+print(directory)
+
+for root, dirs, files in os.walk(directory):
+  for file in files:
+    filepath = os.path.join(root, file)
+    filetime = os.path.getmtime(filepath)
+    formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+    filesize = os.path.getsize(filepath)
+    parent_dir = os.path.dirname(filepath)
+
+    print(f'Обнаружен файл: {file}, '
+          f'Путь: {filepath}, '
+          f'Размер: {filesize} байт, '
+          f'Время изменения: {formatted_time}, '
+          f'Родительская директория: {parent_dir}')
+
+
+
+# for root, dirs, files in os.walk(directory):
+#     for file in files:
+#         filepath = os.path.join(root, file)
+#         filesize = os.path.getsize(filepath)
+#         filetime = os.path.getmtime(filepath)
+#         formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+#         parent_dir = os.path.dirname(filepath)
+#
+#     print(f'File: {filepath}')
+#     print(f'Size: {filesize} bytes')
+#     print(f'Last modified: {formatted_time}')
+#     print(f'Parent directory: {parent_dir}')
+#     print()
+#
+#
+#
+
